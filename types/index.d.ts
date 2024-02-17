4e844a8fb0d0a53229761a95a99bdaba4126b81c
jaimediaz817 <jdsolutions817@gmail.com>
2024-02-17 01:51:14 -0500

    se ajusta fichero de mapeo del enviroment

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,16 +3,26 @@
 declare namespace NodeJS {
   // DECLARACIÓN EXCPLÍCITA DE VARIABLES DE ENTORNO => TIPO
   interface ProcessEnv {
-    APP_PORT:             number;
-    DB_TYPE:              string;
-    DB_HOST:              string;
-    DB_PORT:              number;
-    DB_USER:              string;
-    DB_PASSWORD:          string;
-    DB_NAME:              string;
-    DB_NEST_ENTITY_FILES: string;
-    HASH_SALT:            number;
-    JWT_SECRET:           string;
-    JWT_EXPIRES_IN:       string;
-  }
+        // Postgresql
+        APP_PORT:             number;
+        DB_TYPE:              string;
+        DB_HOST:              string;
+        DB_PORT:              number;
+        DB_USER:              string;
+        DB_PASSWORD:          string;
+        DB_NAME:              string;
+
+        // mysql
+        DB_TYPE_MYSQL:        string;
+        DB_HOST_MYSQL:        string;
+        DB_PORT_MYSQL:        string;
+        DB_USER_MYSQL:        string;
+        DB_PASSWORD_MYSQL:    string;
+        DB_NAME_MYSQL:        string;
+
+        DB_NEST_ENTITY_FILES: string;
+        HASH_SALT:            number;
+        JWT_SECRET:           string;
+        JWT_EXPIRES_IN:       string;
+    }
 }

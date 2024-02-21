fd9b03769950d0b39310b990f0fbe8c2effed3a6
jaimediaz817 <jdsolutions817@gmail.com>
2024-02-21 12:18:53 -0500

    la otra parte de la relación 1 - 1 de ciudad con persona

--- src/user/entities/person.entity.ts
+++ src/user/entities/person.entity.ts
@@ -1,56 +1,64 @@
 import { 
-  Entity, 
-  Column, 
-  PrimaryGeneratedColumn,
-  OneToOne
+    Entity, 
+    Column, 
+    PrimaryGeneratedColumn,
+    OneToOne,
+    JoinColumn,
 } from "typeorm";
 import { UserEntity } from "./user.entity";
+import { CityEntity } from "../../city/entities/city.entity";
 
 
 @Entity('persons')
 export class PersonEntity {
 
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column({nullable: true})
-  full_name: string; // NOMBRE COMPLETO
+    @Column({nullable: true})
+    full_name: string; // NOMBRE COMPLETO
 
-  @Column({nullable: true})
-  first_name: string; // PRIMER NOMBRE
+    @Column({nullable: true})
+    first_name: string; // PRIMER NOMBRE
 
-  @Column({nullable: true})
-  last_name: string; // APELLIDO
+    @Column({nullable: true})
+    last_name: string; // APELLIDO
 
-  // TODO: se agrega el tipo: "CC: Cédula de ciudadanía"
-  @Column({
-    nullable: true,
-    type: 'enum',
-    enum: ['CUIT', 'DNI', 'CC'],
-    default: 'CC'
-  })
-  identification_type: string; 
+    // TODO: se agrega el tipo: "CC: Cédula de ciudadanía"
+    @Column({
+        nullable: true,
+        type: 'enum',
+        enum: ['CUIT', 'DNI', 'CC'],
+        default: 'CC'
+    })
+    identification_type: string; 
 
-  @Column("varchar", { 
-    length: 13 , 
-    nullable: true
-  })
-  identification_number: string;
+    @Column("varchar", { 
+        length: 13 , 
+        nullable: true
+    })
+    identification_number: string;
 
-  @Column({nullable: true})
-  phone_number: string; // TELEFONO
+    @Column({nullable: true})
+    phone_number: string; // TELEFONO
 
-  @Column({nullable: true})
-  cell_number: string; // TELEFONO CELULAR
+    @Column({nullable: true})
+    cell_number: string; // TELEFONO CELULAR
 
-  @Column({
-    type: 'datetime', 
-    default: () => 'CURRENT_TIMESTAMP'
-  })
-  created_at: Date;
-  
-  @OneToOne(() => UserEntity, (user) => user.profile, {
-    onUpdate: 'CASCADE'
-  })
-  user: UserEntity;
+    @Column({
+        type: 'datetime', 
+        default: () => 'CURRENT_TIMESTAMP'
+    })
+    created_at: Date;
+    
+    @OneToOne(() => UserEntity, (user) => user.profile, {
+        onUpdate: 'CASCADE'
+    })
+    user: UserEntity;
+
+    @OneToOne(() => CityEntity, (city) => city.person, {
+        cascade: ['update', 'insert']
+    })
+    @JoinColumn({ name: 'city_id'})
+    city?: CityEntity;  
 }
